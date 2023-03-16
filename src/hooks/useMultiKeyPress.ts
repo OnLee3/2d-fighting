577ac9c14ade675d79a3c0d50df66af8f3c2dd2e
onLee3 <dlwotjr2@gmail.com>
2023-03-16 23:05:12 +0900

    feat: simultaneous key press

--- src/hooks/useMultiKeyPress.ts
+++ src/hooks/useMultiKeyPress.ts
@@ -1,16 +1,50 @@
-import { useKey } from "react-use";
+import { useEffect, useState } from "react";
 
 export const useMultiKeyPress = (
   keys: string[],
   callback: (key: string) => void
 ) => {
-  useKey(
-    (event) => keys.includes(event.key),
-    (event) => {
-      event.preventDefault(); // Prevent browser shortcuts
-      callback(event.key);
-    },
-    { event: "keydown" },
-    [keys, callback]
-  );
+  const [pressedKeys, setPressedKeys] = useState(new Set<string>());
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (keys.includes(event.key) && !pressedKeys.has(event.key)) {
+        setPressedKeys((prevPressedKeys) => {
+          const newPressedKeys = new Set(prevPressedKeys);
+          newPressedKeys.add(event.key);
+          return newPressedKeys;
+        });
+      }
+    };
+
+    const handleKeyUp = (event: KeyboardEvent) => {
+      if (keys.includes(event.key)) {
+        setPressedKeys((prevPressedKeys) => {
+          const newPressedKeys = new Set(prevPressedKeys);
+          newPressedKeys.delete(event.key);
+          return newPressedKeys;
+        });
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [keys, pressedKeys]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      pressedKeys.forEach((key) => {
+        callback(key);
+      });
+    }, 1000 / 60); // Call the callback every frame (60 FPS)
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [pressedKeys, callback]);
 };
