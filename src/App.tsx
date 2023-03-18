f492425548fac18b8c361f0bb533c8420f4bb735
onLee3 <dlwotjr2@gmail.com>
2023-03-18 11:56:38 +0900

    refactor: create gameLogic.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
-import { useMultiKeyPress } from './hooks/useMultiKeyPress';
 import {
     useCharacters,
-    moveCharacter,
-    handleAttack,
     decrementGracePeriods,
     initialCharacters,
 } from './hooks/useCharacters';
 import { drawCharacters } from './utils/drawCharacters';
+import { useGameControls } from './hooks/gameLogic';
 
 const App: React.FC = () => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
     const { characters, setCharacters } = useCharacters();
-    const attackTimeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
     const [winnerIndex, setWinnerIndex] = useState<number | null>(null);
 
     const resetGame = () => {
@@ -50,66 +47,7 @@ const App: React.FC = () => {
         return () => cancelAnimationFrame(animationId);
     }, [gameLoop]);
 
-    useMultiKeyPress([
-        {
-            keys: ['a', 'd', 'ArrowLeft', 'ArrowRight'],
-            callback: (key: string) => {
-                switch (key) {
-                    case 'a':
-                        moveCharacter(0, -5, setCharacters);
-                        break;
-                    case 'd':
-                        moveCharacter(0, 5, setCharacters);
-                        break;
-                    case 'ArrowLeft':
-                        moveCharacter(1, -5, setCharacters);
-                        break;
-                    case 'ArrowRight':
-                        moveCharacter(1, 5, setCharacters);
-                        break;
-                    default:
-                        break;
-                }
-            },
-        },
-        {
-            keys: ['q', 'p'],
-            callback: (key) => {
-                switch (key) {
-                    case 'q':
-                        handleAttack(0, 'right', setCharacters, () => {
-                            if (attackTimeoutRef.current) {
-                                clearTimeout(attackTimeoutRef.current);
-                            }
-                            attackTimeoutRef.current = setTimeout(() => {
-                                setCharacters((prevCharacters) => {
-                                    const newCharacters = [...prevCharacters];
-                                    newCharacters[0].attacking = false;
-                                    return newCharacters;
-                                });
-                            }, 300);
-                        });
-                        break;
-                    case 'p':
-                        handleAttack(1, 'left', setCharacters, () => {
-                            if (attackTimeoutRef.current) {
-                                clearTimeout(attackTimeoutRef.current);
-                            }
-                            attackTimeoutRef.current = setTimeout(() => {
-                                setCharacters((prevCharacters) => {
-                                    const newCharacters = [...prevCharacters];
-                                    newCharacters[1].attacking = false;
-                                    return newCharacters;
-                                });
-                            }, 300);
-                        });
-                        break;
-                    default:
-                        break;
-                }
-            },
-        },
-    ]);
+    useGameControls(setCharacters);
 
     return (
         <div className="App">
